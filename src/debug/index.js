15267299a37252927cc216fb7ee92732370a874a
Seba Sanchez <>
2021-08-12 21:41:35 -0400

    Class 11, chrome debugger

---
+++ src/debug/index.js
@@ -0,0 +1,15 @@
+var a = 'hello';
+
+function hello() {
+  let b = 'World';
+  const c = 'Hello World!';
+
+  if (true) {
+    let d = 'Hello World!!!'
+    debugger;
+  } 
+}
+
+hello();
+
+//* copy the code above in chrome console, and it will pause in the 'debugger' line (9)
